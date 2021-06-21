3e9f0b5394efec7e7966976012cad9b7eb482ffd
Malo Mn <malo.maisonneuve@gmail.com>
2021-06-21 21:37:28 +0200

    Transaction, Income & Outcome objects

---
+++ Transaction.py
@@ -0,0 +1,13 @@
+class Transaction:
+    def __init__(money, ):
+        self.money = money
+
+
+class Income(Transation):
+    def __init__():
+        pass
+
+
+class Outcome(Transation):
+    def __init__():
+        pass
