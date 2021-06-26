92429b235ee5bde8b223149d9c3f22e4ece86832
Malo Mn <malo.maisonneuve@gmail.com>
2021-06-26 15:12:03 +0200

    Parser skeleton & completed gitignore

--- parser.py
+++ parser.py
@@ -1,6 +1,10 @@
-def parse_file(filename):
-    pass
+import openpyxl
+
+
+def parse_xlsx_file(filename):
+    book = openpyxl.load_workbook('sample.xlsx')
+    print(book.active['A1'])
 
 
 if __name__ == "__main__":
-    parse_file()
+    parse_xlsx_file('data/CA20210624_224522.xlsx')
